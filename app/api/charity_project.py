ca24780ac1e83880305f45b8ef0c52eee79cefb7
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-04-12 22:54:17 +0300

    Depends added

--- app/api/charity_project.py
+++ app/api/charity_project.py
@@ -1,10 +1,12 @@
 # app/api/meeting_room.py
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
 from app.crud.charity_project import create_charity_project
 from app.schemas.charity_project import CharityProjectCreate, CharityProjectDB
 from app.crud.charity_project import create_charity_project, get_project_id_by_name
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.db import get_async_session
 
 
 router = APIRouter()
@@ -17,12 +19,13 @@ router = APIRouter()
 )
 async def create_new_charity_project(
         charity_project: CharityProjectCreate,
+        session: AsyncSession = Depends(get_async_session),
 ):
-    project_id = await get_project_id_by_name(charity_project.name)
+    project_id = await get_project_id_by_name(charity_project.name, session)
     if project_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Проект с таким именем уже существует!',
         )
-    new_project = await create_charity_project(charity_project)
+    new_project = await create_charity_project(charity_project, session)
     return new_project
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -6,28 +6,31 @@ from app.models.charity_project import CharityProject
 from app.schemas.charity_project import CharityProjectCreate
 from sqlalchemy import select
 from typing import Optional
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def create_charity_project(
-        new_project: CharityProjectCreate
+        new_project: CharityProjectCreate,
+        session: AsyncSession,
 ) -> CharityProject:
     new_project_data = new_project.dict()
     db_project = CharityProject(**new_project_data)
-    async with AsyncSessionLocal() as session:
-        session.add(db_project)
-        await session.commit()
-        await session.refresh(db_project)
+    session.add(db_project)
+    await session.commit()
+    await session.refresh(db_project)
     return db_project
 
 
-async def get_project_id_by_name(charity_project: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        db_project_id = await session.execute(
-            select(CharityProject.id).where(
-                CharityProject.name == charity_project
-            )
+async def get_project_id_by_name(
+    charity_project: str,
+    session: AsyncSession,
+) -> Optional[int]:
+    db_project_id = await session.execute(
+        select(CharityProject.id).where(
+            CharityProject.name == charity_project
         )
-        db_project_id = db_project_id.scalars().first()
+    )
+    db_project_id = db_project_id.scalars().first()
     return db_project_id
 
 
